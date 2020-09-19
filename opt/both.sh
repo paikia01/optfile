94d959fe671ee453ca9b77cc3596fff9626ee89e
paikia01 <1226447825@qq.com>
2020-09-19 23:49:20 +0800

    Update both.sh

--- opt/both.sh
+++ opt/both.sh
@@ -1,25 +1,27 @@
 #!/bin/sh
 
-killall wget
-rm -rf /opt*
+ if [ ! -d "/opt/bin" ]; then
+  mount -t tmpfs -o size=24m tmpfs /opt
+  else
+  echo "[opt is mount  !  !  !]"
+ fi
 
-if [ -d "/opt" ]; then
-  mount -t tmpfs -o size=20m tmpfs /opt
-  sleep 15
-  cd /opt
-  wget -O /opt/opt.tar https://cdn.jsdelivr.net/gh/paikia01/optfile/opt/opttmpg7.tgz 
+cd /opt
+wget -O /opt/opt.tar https://cdn.jsdelivr.net/gh/paikia01/optfile/opt/opttmpg7.tgz
+tar -zxf opt.tar
+rm -rf opt.tar
+logger -t "【opt环境】：安装完成"
+if [ $? = 0 ]; then
+ opkg update 
+fi
+logger -t "【opt环境】：已更新"
+
+logger -t "【opt】：正在安装ttyd"
+if [ $? = 0 ]; then
+opkg install ttyd
 fi
 
-if [$? = 0] ; then
-	sleep 5
-	cd /opt
-	tar -zxvf opt.tar
-	rm -rf opt.tar
-	opkg update && opkg install wget ttyd
-	logger -t "【wget】：已更新"
-	logger -t "【opt环境】：安装完成"
-	/opt/bin/ttyd -p 4200 bash >/dev/null 2>&1 & 
-	echo "[opkg update]"
-	echo "[opkg install wget ttyd]"
-	logger -t "【ttyd】：启动成功"
+if [ $? = 0 ]; then
+ /opt/bin/ttyd -p 4200 bash >/dev/null 2>&1 &
 fi
+logger -t "【ttyd】：启动成功"
